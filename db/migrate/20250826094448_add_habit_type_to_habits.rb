2991bfca81ccf5eaa6ce0704326707dd9cfeac39
clement jacquet <clement.jacquet.16@gmail.com>
2025-08-26 12:03:59 +0200

    added seeding, changed schema, did habit.show

---
+++ db/migrate/20250826094448_add_habit_type_to_habits.rb
@@ -0,0 +1,5 @@
+class AddHabitTypeToHabits < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :habits, :habit_type, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,32 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+HabitType.destroy_all
+Habit.destroy_all
+
+p "started seeding"
+
+user = User.create!(
+  email: "tata@tot.fr",
+  password: "123456",
+  pseudo: "tata",
+  avatar: "oooo",
+  age: 30,
+  location: "BDX"
+)
+habit_type = HabitType.create!(
+  name: "cigarette",
+  unit: "cigarettes",
+  verb: "arrêter"
+)
+p habit_type
+
+habit = Habit.create!(
+  user: user,
+  name: "Arrêter de fumer dans 6",
+  habit_type: habit_type,
+  visibility: "public",
+  category: "Santé"
+)
+
+p habit
+
+p "finished seeding"
