b5632f0c083d7e850894454faab957c05f0be446
clement jacquet <clement.jacquet.16@gmail.com>
2025-08-28 17:50:46 +0200

    fixed sign in and sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    redirect_to new_user_session_path unless user_signed_in?
     @habits = Habit.all
     @categories = Category.all
     @habit_types = HabitType.all
