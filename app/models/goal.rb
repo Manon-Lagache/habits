10f18485f94251b6db50ba959a82adebe0d708a3
clement jacquet <clement.jacquet.16@gmail.com>
2025-09-01 16:44:14 +0200

    added challenge to home html

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -6,4 +6,15 @@ class Goal < ApplicationRecord
   validates :end_type, presence: true
 
   store_accessor :tracking_config, :weekly_days, :monthly_days, :monthly_count, :reminder
+
+  def translate_frequency
+    case self.frequency
+    when "daily"
+      "par jour"
+    when "weekly"
+      "par semaine"
+    when "monthly"
+      "par mois"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :habits, dependent: :destroy
+  has_many :goals, through: :habits, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :group_memberships, dependent: :destroy
   has_many :groups, through: :group_memberships
+  has_many :challenges, dependent: :destroy
 
   def available_challenges
     participated_challenge_ids = groups.joins(:challenge).pluck('challenges.id')
     Challenge.where.not(id: participated_challenge_ids)
+  end
+
+  def unavailable_challenges
+    participated_challenge_ids = groups.joins(:challenge).pluck('challenges.id')
+    Challenge.where(id: participated_challenge_ids)
+  end
+
+  def display_challenges
+    unavailable_challenges
+  end
+
+  def display_challenge_users
   end
 end
