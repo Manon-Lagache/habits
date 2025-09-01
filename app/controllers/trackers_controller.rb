561e636498c364ce23c904d27cbb6debc2c32cfe
guillem delpuech <delpuechguillem98@gmail.com>
2025-09-01 16:19:56 +0200

    graph connecté à create de tracker

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -7,7 +7,7 @@ class TrackersController < ApplicationController
   def create
     trackers = tracker_params
     trackers.each do |tracker|
-      Tracker.create(value: tracker.last.require(:value), habit_id: tracker.last.require(:habit_id))
+      Tracker.create(value: tracker.last.require(:value), habit_id: tracker.last.require(:habit_id), date: Date.today)
     end
     redirect_to root_path
   end
