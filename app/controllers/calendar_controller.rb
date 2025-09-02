b3b093e9ccd5266401b301df69dd896abfa9c7d8
Annouchka Vandeville <annouchka.vandeville@kedgebs.com>
2025-09-02 10:01:17 +0200

    fichier supprimé

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,5 @@
 class CalendarController < ApplicationController
   def index
-    # Déterminer le mois affiché
     if params[:month] && params[:year]
       @current_month = Date.new(params[:year].to_i, params[:month].to_i)
     else
@@ -12,14 +11,12 @@ class CalendarController < ApplicationController
 
     @habits = current_user.habits.includes(:goal)
 
-    # Initialisation du hash pour le calendrier
     @calendar_habits = Hash.new { |hash, key| hash[key] = [] }
 
     @habits.each do |habit|
       goal = habit.goal
       next unless goal.present?
 
-      # Définir début et fin selon end_type
       case goal.end_type
       when "period"
         debut = goal.start_date.presence || habit.created_at.to_date
@@ -35,10 +32,8 @@ class CalendarController < ApplicationController
         fin   = habit.created_at.to_date
       end
 
-      # Sécurité : on ne met que si début et fin sont valides
       next if debut.nil? || fin.nil? || fin < debut
 
-      # Remplir le calendrier pour chaque jour entre début et fin
       (debut..fin).each do |date|
         @calendar_habits[date] << habit
       end
