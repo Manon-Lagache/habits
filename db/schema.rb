bb4e27e0893148868891095cd29164315656dc7e
Manon Lagache <manlagache@hotmail.fr>
2025-09-03 12:06:21 +0200

    add column xpreward to users + relation db challenges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_03_090900) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_03_100143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,6 +124,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_03_090900) do
     t.string "avatar"
     t.integer "age"
     t.string "location"
+    t.integer "xp_reward"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
