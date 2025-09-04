80ae22e856107868b4589237b6760014bcba9210
Annouchka Vandevile <annouchka.vandeville@kedgebs.com>
2025-09-04 09:46:41 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     @user = current_user
     @tips = Tip.all
     @daily_tips = Tip.where(tip_type: "daily", habit: @habits) 
-    @trackers = @habits.map{|h| h.trackers.build} #entre debut et fin avec date today
+    @trackers = @habits.select{|h| h.has_goal_for_date?(Date.today)}.map{|h| h.trackers.build}
     @habit = Habit.new
     @habit.build_goal
     @challenges = current_user.display_challenges
