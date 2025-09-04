91857e869f895edf95acdf7ba531d0fce57bc057
Manon Lagache <manlagache@hotmail.fr>
2025-09-04 23:20:38 +0200

    essaie de Faire en sorte que l’avatar qui organise l’event soit le même sur la show event que sur la home quand on veut y participer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
-
   def home
     @categories = Category.all
     @habit_types = HabitType.all
-    @habits = current_user.habits.order(created_at: :desc) 
+    @habits = current_user.habits.order(created_at: :desc)
     @user = current_user
     @tips = Tip.all
-    @daily_tips = Tip.where(tip_type: "daily", habit: @habits) 
+    @daily_tips = Tip.where(tip_type: "daily", habit: @habits)
     @trackers = @habits.select{|h| h.has_goal_for_date?(Date.today)}.map{|h| h.trackers.build}
     @habit = Habit.new
     @habit.build_goal
